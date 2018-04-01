2f1280a1819d07cdb6d4744ff684016ca844ba5d
Ryan Woodcock <EpicSquid@users.noreply.github.com>
2018-04-01 22:04:48 +1000

    Updated server page

--- html/index.php
+++ html/index.php
@@ -77,38 +77,44 @@ if($serverSock !== FALSE)
         <div id="main">
           <h2>General information</h2>
           <p>
-            The testing server is always running the latest development version of EnderIO, so be aware that the server routinely will restart to update to the latest version.<br />
+            The testing server is always running the latest development version of EnderIO, so be aware that the server routinely will restart to update to the latest version.<br>
             <i>The world will only be deleted should the mod require that it be.</i>
+	    <br>
           </p>
           <h2>Reporting bugs</h2>
           <p>
-            Play and have fun, but remember to report the bugs you discover.<br />
-            All bugs should be reported in #server-bugreports at EnderIO's Discord server.
-            <div id="discord">
+            Play and have fun, but remember to report the bugs you discover.<br>
+            All bugs should be reported in #bug-reports at EnderIO's Discord server.
+            </p><div id="discord">
               <div id="discord-logo">
-                <img src="assets/discord.png" />
+                <img src="assets/discord.png">
               </div>
               <div>
                 <a href="http://discord.gg/sgYk3Jr">Join Discord</a>
               </div>
             </div>
-          </p>
-         <!-- <p class="callout">
-            <strong>Before reporting any bugs,</strong> please search and familiarise yourself with the issues that have already been reported.
-          </p> -->
+          <p></p>
           <h2>Dependencies</h2>
           <p>
-            We automatically generate an always up-to-date zip for MultiMC that contains all the dependencies.
+            We automatically generate an always up-to-date zip for MultiMC that contains all the dependencies. This instance will crash on the first load due to the updater. Each time you launch the instance it will be updated to use the latest version of the server pack, including mods and configs.
           </p>
           <p><strong><a id="mufile" href="enderiotest_multimc.zip">enderiotest_multimc.zip</a></strong></p>
-                  <br />
-          <p><i>MultiMC is a free, open source launcher for Minecraft. It allows you to have multiple separated instances of Minecraft. Find out more about MultiMC at <a href="https://multimc.org/#Download">the MultiMC project page</a></i><br /></p>
-                  <p>Getting started: <a href="https://github.com/MultiMC/MultiMC5/wiki/Getting-Started">https://github.com/MultiMC/MultiMC5/wiki/Getting-Started</a></p><br />
-          <p>The server is running <span class="highlight">Minecraft 1.12.2</span> with <span class="highlight">forge-1.12.2-14.23.2.2613</span></p>
-          <ul>
-			$modEntries
-          </ul>
-          <p><i>Please note that the links are generated automatically. If your jar files are outdated, you can visit this page.</i></p>
+                  <br>
+          <p><i>MultiMC is a free, open source launcher for Minecraft. It allows you to have multiple separated instances of Minecraft. Find out more about MultiMC at <a href="https://multimc.org/#Download">the MultiMC project page</a></i><br></p>
+                  <p>Getting started: <a href="https://github.com/MultiMC/MultiMC5/wiki/Getting-Started">https://github.com/MultiMC/MultiMC5/wiki/Getting-Started</a></p><br>
+          <p>The server is running <span class="highlight">Minecraft 1.12.2</span> with <span class="highlight">forge-1.12.2-14.23.2.2624</span></p>
+	      <h2>Rules</h2>
+		<p>Play nice, moderators word is law. Do not use item duplication bugs. Do not abuse AFK tricks for resources. You will at least be kicked, and most likely banned if found doing these things.</p>
+	  <h2>Suggestions</h2>
+	    <p>If you have any mod suggestions, they must fulfill at least one of the following guidelines:</p>
+	      <ul>
+	        <li>Ender IO or Ender Zoo has specific integration with that mod.</li>
+	        <li>The mod has specific integration with Ender IO or a has a good candidate for it</li>
+	        <li>The mod enables testing a specific functionality of Ender IO</li>
+	        <li>The mod provides a replacement for a currently broken/missing feature of Ender IO</li>
+	        <li>The mod helps keep the server running</li>
+	      </ul>
+            <p>The mod must also be publicly available for Minecraft 1.12.2. Remember, no begging.</p> 
         </div>
       </div>
       <div id="margin-fix">&nbsp;</div>
